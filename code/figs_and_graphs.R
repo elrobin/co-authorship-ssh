0fbad9911d9295b41eff33c0a5970fd56b8e68d9
Nicolás Robinson García <nirogar@upvnet.upv.es>
2016-12-02 12:54:25 +0100

    Bar plot pubs by year

--- code/figs_and_graphs.R
+++ code/figs_and_graphs.R
@@ -2,9 +2,11 @@
 # Figuras y tablas - Coautoría ANECA
 #-------------------
 
-# Import data
-
+# Load libraries needed
+library(ggplot2)
 library(readr)
+
+# Import data
 data <- read_delim("C:/Users/nirogar/Google Drive/1. Work sync/Work in progress/co-authorship-ssh/data/2000_2013.txt",
 "\t", escape_double = FALSE, na = "NULL",
 trim_ws = TRUE)
@@ -16,5 +18,10 @@ data$py <- as.factor(data$py) # Convert py to factors
 
 # Contingency table with n publications by wc and year
 pubs.py.wc <- table(wc, py)
+pubs.py <- table(py)
 
-plot(pubs.py.wc)
+# Publicaciones por año
+pubs.py <- as.data.frame(pubs.py)
+g <- ggplot(pubs.py, aes(py, weight = Freq))
+g+geom_bar()
+
